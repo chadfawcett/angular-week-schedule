1d882c65746bd71d8e71d074e8f483752f4d9e40
Chad Fawcett <me@chadf.ca>
2015-09-01 00:24:02 -0700

    Ensure example dates start on monday and don't go past friday

--- example/script.js
+++ example/script.js
@@ -2,63 +2,63 @@ angular.module('weekScheduleTest', ['cfWeekSchedule'])
 .controller('weekScheduleTestCtrl', function ($scope) {
   $scope.events = [
     {
-      start: moment().hour(8).minute(30).second(0).millisecond(0),
-      end: moment().hour(9).minute(20).second(0).millisecond(0),
+      start: moment().day(1).hour(8).minute(30).second(0).millisecond(0),
+      end: moment().day(1).hour(9).minute(20).second(0).millisecond(0),
       title: 'Comp 1230'
     },
     {
-      start: moment().hour(11).minute(30).second(0).millisecond(0),
-      end: moment().hour(12).minute(20).second(0).millisecond(0),
+      start: moment().day(1).hour(11).minute(30).second(0).millisecond(0),
+      end: moment().day(1).hour(12).minute(20).second(0).millisecond(0),
       title: 'Math 1380'
     },
     {
-      start: moment().hour(12).minute(0).second(0).millisecond(0),
-      end: moment().hour(13).minute(30).second(0).millisecond(0),
+      start: moment().day(1).hour(12).minute(0).second(0).millisecond(0),
+      end: moment().day(1).hour(13).minute(30).second(0).millisecond(0),
       title: 'Math 1390'
     },
     {
-      start: moment().hour(15).minute(30).second(0).millisecond(0),
-      end: moment().hour(17).minute(0).second(0).millisecond(0),
+      start: moment().day(1).hour(15).minute(30).second(0).millisecond(0),
+      end: moment().day(1).hour(17).minute(0).second(0).millisecond(0),
       title: 'Comp 2230'
     },
     {
-      start: moment().hour(11).minute(30).second(0).millisecond(0).add(1, 'day'),
-      end: moment().hour(12).minute(20).second(0).millisecond(0).add(1, 'day'),
+      start: moment().day(1).hour(11).minute(30).second(0).millisecond(0).add(1, 'day'),
+      end: moment().day(1).hour(12).minute(20).second(0).millisecond(0).add(1, 'day'),
       title: 'Math 1380'
     },
     {
-      start: moment().hour(15).minute(30).second(0).millisecond(0),
-      end: moment().hour(16).minute(20).second(0).millisecond(0),
+      start: moment().day(1).hour(15).minute(30).second(0).millisecond(0),
+      end: moment().day(1).hour(16).minute(20).second(0).millisecond(0),
       title: 'Cmns 1290'
     },
     {
-      start: moment().hour(16).minute(0).second(0).millisecond(0),
-      end: moment().hour(16).minute(45).second(0).millisecond(0),
+      start: moment().day(1).hour(16).minute(0).second(0).millisecond(0),
+      end: moment().day(1).hour(16).minute(45).second(0).millisecond(0),
       title: 'Cmns 1290'
     },
     {
-      start: moment().hour(8).minute(30).second(0).millisecond(0).add(1, 'day'),
-      end: moment().hour(9).minute(20).second(0).millisecond(0).add(1, 'day'),
+      start: moment().day(1).hour(8).minute(30).second(0).millisecond(0).add(1, 'day'),
+      end: moment().day(1).hour(9).minute(20).second(0).millisecond(0).add(1, 'day'),
       title: 'Comp 1230'
     },
     {
-      start: moment().hour(12).minute(0).second(0).millisecond(0).add(2, 'days'),
-      end: moment().hour(13).minute(30).second(0).millisecond(0).add(2, 'days'),
+      start: moment().day(1).hour(12).minute(0).second(0).millisecond(0).add(2, 'days'),
+      end: moment().day(1).hour(13).minute(30).second(0).millisecond(0).add(2, 'days'),
       title: 'Math 1390'
     },
     {
-      start: moment().hour(15).minute(30).second(0).millisecond(0).add(3, 'days'),
-      end: moment().hour(17).minute(0).second(0).millisecond(0).add(3, 'days'),
+      start: moment().day(1).hour(15).minute(30).second(0).millisecond(0).add(3, 'days'),
+      end: moment().day(1).hour(17).minute(0).second(0).millisecond(0).add(3, 'days'),
       title: 'Comp 2230'
     },
     {
-      start: moment().hour(15).minute(30).second(0).millisecond(0).add(4, 'days'),
-      end: moment().hour(16).minute(20).second(0).millisecond(0).add(4, 'days'),
+      start: moment().day(1).hour(15).minute(30).second(0).millisecond(0).add(4, 'days'),
+      end: moment().day(1).hour(16).minute(20).second(0).millisecond(0).add(4, 'days'),
       title: 'Cmns 1290'
     },
     {
-      start: moment().hour(16).minute(0).second(0).millisecond(0).add(4, 'days'),
-      end: moment().hour(16).minute(45).second(0).millisecond(0).add(4, 'days'),
+      start: moment().day(1).hour(16).minute(0).second(0).millisecond(0).add(4, 'days'),
+      end: moment().day(1).hour(16).minute(45).second(0).millisecond(0).add(4, 'days'),
       title: 'Cmns 1290'
     }
   ]
